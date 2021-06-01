4c1ba1e3b9e6f9230e77b7f6306826559fadb857
Thomas Schweizer <tschweiz@cs.washington.edu>
2021-06-01 16:57:48 -0700

    Implement links

--- docs/js/visualization.js
+++ docs/js/visualization.js
@@ -8,167 +8,126 @@ main();
 
 async function main() {
   const jsonData = await d3.json("./data/args4j.json");  
-  var args4jData = processJson(jsonData);
+  let classes = jsonData.classNames
+  let data = processMethodData(jsonData);
+  // let data = sampleData;
 
-  // Import data
-let data = args4jData;
+  console.log({data})
 
-let classes = args4jData.classNames;
+  // Pick a center node for the visualization which will represent the local context.
+  let centerNodeId = data.nodes[Math.floor(Math.random() * data.nodes.length)];
+  console.log(centerNodeId.id)
 
-console.log({args4jData})
-console.log({data})
-
-// Pick a center node for the visualization which will represent the local context.
-let centerNodeId = data.nodes[Math.floor(Math.random() * data.nodes.length)];
-console.log(centerNodeId.fqn)
-
-// Visualization configuration
+  // Visualization configuration
 
 
-// var width = 500;
-// var height = 500; 
-var width = container.clientWidth;
-var height = container.clientHeight; 
+  var width = container.clientWidth;
+  var height = container.clientHeight; 
+  var width = 2000;
+  var height = 2000; 
 
 
-var color = d3.scaleOrdinal()
-  .domain(classes)
-  .range(d3.schemeSet3);
+  var color = d3.scaleOrdinal()
+    .domain(classes)
+    .range(d3.schemeSet3);
 
-const svg = d3.select("#method-level")
-  .append("svg")
-  .attr("width", container.clientWidth)
-  .attr("height", container.clientHeight)
-  // .attr("viewBox", [0, 0, width, height]);
+  const svg = d3.select("#method-level")
+    .append("svg")
+    .attr("width", container.clientWidth)
+    .attr("height", container.clientHeight)
+    .attr("viewBox", [0, 0, width, height]);
 
-// const link = svg
-//   .selectAll(".link")
-//   .data(data.links)
-//   .join("line")
-//     .classed("link", true);
+  const link = svg
+    .selectAll(".link")
+    .data(data.links)
+    .join("line")
+    .classed("link", true);
 
-const node = svg
-  .selectAll(".node")
-  .data(data.nodes)
-  .join("circle")
+  const node = svg
+    .selectAll(".node")
+    .data(data.nodes)
+    .join("circle")
     .attr("r", 6)
     .attr("fill", d => color(d.class))
     .classed("node", true)
     .classed("fixed", d => d.fx !== undefined);
 
-node
-  .on('mouseover', function(e, d) {
-    tooltip
-      .text(d.name)
-      .attr('x', e.x)
-      .attr('y', e.y - 50) // Why -50?
-  })
-  .on('mouseout', function() {
-    tooltip.text("")
-  });
+  const centerNode = node
+    .filter(function(d) {
+      return d.id === centerNodeId.id})
+    .classed("fixed", true)
+    .attr("x", 0)
+    .attr("y", 0)
+    .attr("r", 20);
 
-const centerNode = node
-  .filter(function(d) {
-    return d.fqn === centerNodeId.fqn})
-  .classed("fixed", true)
-  .attr("x", 0)
-  .attr("y", 0)
-  .attr("r", 20);
+  const tooltip = svg.append('text')
+  .attr('x', 10)
+  .attr('y', height - 10);
 
-const tooltip = svg.append('text')
-.attr('x', 10)
-.attr('y', height - 10);
+  const simulation = d3
+    .forceSimulation()
+    .nodes(data.nodes)
+    .force("charge", d3.forceManyBody(20))
+    .force("center", d3.forceCenter(width / 2, height / 2))
+    // .force('collision', d3.forceCollide().radius(function(d) {
+    //   if(d.id === centerNodeId.id) {
+    //     return 20;
+    //   }
+    //   return 2;
+    // }))
+    .force("link", d3.forceLink(data.links))
+    .on("tick", tick);
 
-const simulation = d3
-  .forceSimulation()
-  .nodes(data.nodes)
-  .force("center", d3.forceCenter(width / 2, height / 2))
-  .force('collision', d3.forceCollide().radius(function(d) {
-    if(d.fqn === centerNodeId.fqn) {
-      return 20;
-    }
-    return 2;
-  }))
-  // .force("cluster", forceCluster())
-  // .force("charge", d3.forceManyBody().strength(-1 ))
-  // .force("link", d3.forceLink(data.links))
-  .on("tick", tick);
+    node.on('mouseover', function(e, d) {
+      tooltip
+        .text(d.name)
+    })
+    .on('mouseout', function() {
+      tooltip.text("")
+    });
 
-const drag = d3
-  .drag()
-  .on("start", dragstart)
-  .on("drag", dragged);
+  const drag = d3
+    .drag()
+    .on("start", dragstart)
+    .on("drag", dragged);
 
-node.call(drag).on("click", click);
+  node.call(drag).on("click", click);
 
-function tick() {
-  // link
-  //   .attr("x1", d => d.source.x)
-  //   .attr("y1", d => d.source.y)
-  //   .attr("x2", d => d.target.x)
-  //   .attr("y2", d => d.target.y);
+  function tick() {
+    
+    link
+      .attr("x1", d => d.source.x)
+      .attr("y1", d => d.source.y)
+      .attr("x2", d => d.target.x)
+      .attr("y2", d => d.target.y);
 
-  node
-    .attr("cx", d => d.x)
-    .attr("cy", d => d.y);
-}
-
-function forceCluster() {
-  //  Credits to https://observablehq.com/@d3/clustered-bubbles
-
-  const strength = 0.2;
-  let nodes;
-
-  function force(alpha) {
-
-    const centroids = d3.rollup(nodes, centroid, d => d.class);
-    const l = alpha * strength;
-    for (const d of nodes) {
-      const {x: cx, y: cy} = centroids.get(d.class);
-      d.vx -= (d.x - cx) * l;
-      d.vy -= (d.y - cy) * l;
-    }
+    node
+      .attr("cx", d => d.x)
+      .attr("cy", d => d.y);
   }
 
-  force.initialize = _ => nodes = _;
-
-  return force;
-}
-
-function centroid(nodes) {
-  let x = 0;
-  let y = 0;
-  let z = 0;
-  for (const d of nodes) {
-    let k = d.r ** 2;
-    x += d.x * k;
-    y += d.y * k;
-    z += k;
+  function click(event, d) {
+    delete d.fx;
+    delete d.fy;
+    d3.select(this).classed("fixed", false);
+    simulation.alpha(1).restart();
   }
-  return {x: x / z, y: y / z};
-}
 
-function click(event, d) {
-  delete d.fx;
-  delete d.fy;
-  d3.select(this).classed("fixed", false);
-  simulation.alpha(1).restart();
-}
+  function dragstart() {
+    d3.select(this).classed("fixed", true);
+  }
 
-function dragstart() {
-  d3.select(this).classed("fixed", true);
-}
+  function dragged(event, d) {
+    d.fx = clamp(event.x, 0, width);
+    d.fy = clamp(event.y, 0, height);
+    simulation.alpha(1).restart();
+  }
 
-function dragged(event, d) {
-  d.fx = clamp(event.x, 0, width);
-  d.fy = clamp(event.y, 0, height);
-  simulation.alpha(1).restart();
-}
+  function clamp(x, lo, hi) {
+    return x < lo ? lo : x > hi ? hi : x;
+  }
 
-function clamp(x, lo, hi) {
-  return x < lo ? lo : x > hi ? hi : x;
+
 }
 
 // Template code from https://observablehq.com/@d3/sticky-force-layout?collection=@d3/d3-force
-}
-
